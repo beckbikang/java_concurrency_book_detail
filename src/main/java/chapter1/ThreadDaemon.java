9e8f49890baeb557a97c4530b13424215383904e
bikang <bikang1@staff.weibo.com>
2018-08-11 18:22:45 +0800

    新增代码

--- src/main/java/chapter1/ThreadDaemon.java
+++ src/main/java/chapter1/ThreadDaemon.java
@@ -5,10 +5,11 @@ import util.Sleeper;
 import java.util.ArrayDeque;
 import java.util.Date;
 import java.util.Deque;
+import java.util.NoSuchElementException;
 
 /**
- *  1 不停的写入数据
- *  2 启动线程不停的清楚不需要的数据
+ *  1 启动几个线程不停的写入数据
+ *  2 启动线程不停的处理超时数据
  *
  */
 public class ThreadDaemon {
@@ -17,32 +18,44 @@ public class ThreadDaemon {
     public static void main(String[] args) throws Exception{
         Deque<Event> queues  = new ArrayDeque<Event>();
 
-
-
         for(int j = 0; j < 3;j ++){
-            Task task = new Task(String.format("thread-%d",j), queues);
-            task.start();
+            Thread thread = new Thread(new PutTask("thread-%d"+j, queues));
+            thread.start();
         }
-        for (int i = 0; i < 10;i++){
-            Date date = new Date();
-            Event event = new Event();
-            event.setDate(date);
-            event.setName("number"+i);
-            queues.push(event);
-            Sleeper.sleep(1);
-            System.out.println("put event "+date);
-        }
-        System.out.println("over");
-        Sleeper.sleep(5);
-    }
 
+        CleanTask cleanTask= new CleanTask("clean1",queues);
+        cleanTask.start();
+
+        cleanTask.join();
+        System.out.println("over");
+    }
+}
+
+class PutTask implements Runnable{
+
+    private Deque<Event> queue;
+    private String name;
+    public PutTask(String name ,Deque<Event> q){
+        this.name = name;
+        queue = q;
+    }
+    public void run() {
+        for(int i= 0;i < 100 ;i++){
+            Event event = new Event();
+            event.setName(name);
+            event.setDate(new Date());
+            queue.addFirst(event);
+            System.out.printf("put %s : %s\n", Thread.currentThread().getName(), event.getDate());
+            Sleeper.sleep(1);
+        }
+    }
 }
 
 
-class Task extends Thread{
+class CleanTask extends Thread{
     private Deque<Event> queue;
 
-    public Task(String name, Deque<Event> q){
+    public CleanTask(String name, Deque<Event> q){
         super(name);
         queue = q;
         this.setDaemon(true);
@@ -52,32 +65,42 @@ class Task extends Thread{
 
         while (true){
             Date date = new Date();
-
             clean(date);
         }
     }
 
-    //clear data
+    /**
+     * 定时删除过期的时间
+     * @param date
+     */
     public void clean(Date date){
         if(queue.isEmpty()){
             return;
         }
         long scap = 0l;
+        boolean flag = false;
         do {
-            Event event = queue.getFirst();
-            scap = event.getDate().getTime() - date.getTime();
-            System.out.println("first" + event.getDate());
-
-            System.out.println(queue.getLast().getDate());
-            System.out.println(scap);
+            if (queue.isEmpty()){
+                return;
+            }
+            Event event;
+            try {
+                event = queue.getLast();
+            }catch (NoSuchElementException e){
+                return;
+            }
+            scap = date.getTime()- event.getDate().getTime();
+            //System.out.printf("cap=%d\n",scap);
             if (scap > 3000) {
-                System.out.printf("%s : %s\n", Thread.currentThread().getName(), event.getDate());
-                queue.removeFirst();
-            } else {
-                //System.out.println("end");
-                break;
+                queue.removeLast();
+                System.out.printf("delete %s :cur=%s delete=%s\n",
+                        Thread.currentThread().getName(),date, event.getDate());
+                flag = true;
             }
         }while ( scap > 3000);
+        if (flag){
+            System.out.printf("queue_size=%d\n", queue.size());
+        }
     }
 
 }
