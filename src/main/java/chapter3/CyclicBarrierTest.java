a93316ada8fa3f0496ebd84767a545358359a5c1
bikang <bikang1@staff.weibo.com>
2018-08-13 15:05:10 +0800

    CyclicBarrier use

--- src/main/java/chapter3/CyclicBarrierTest.java
+++ src/main/java/chapter3/CyclicBarrierTest.java
@@ -9,26 +9,30 @@ import java.util.concurrent.CyclicBarrier;
 public class CyclicBarrierTest {
 
     public static void main(String[] args){
-        Meeting meeting = new Meeting(10);
 
-        Thread thread1 = new Thread(meeting);
-        thread1.start();
-        for(int i  =0;i < 10;i++){
+        int num = 3;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(
+                num,
+                new LastRunner());
+        MeetingCyclicBarrier meeting =
+                new MeetingCyclicBarrier(cyclicBarrier);
+        for(int i  =0;i < num;i++){
             Thread thread = new Thread(
-                    new Parter("thread:"+i,meeting)
+                    new ParterCyclicBarrier("thread:"+i,meeting)
             );
             thread.start();
         }
-        Sleeper.sleep(5);
+        Sleeper.sleep(2);
+        System.out.println("over");
     }
 }
 
 
 class MeetingCyclicBarrier implements Runnable{
 
-    CyclicBarrier cyclicBarrier;
-    MeetingCyclicBarrier(int n){
-        cyclicBarrier = new CyclicBarrier(n);
+    private final CyclicBarrier cyclicBarrier;
+    MeetingCyclicBarrier(CyclicBarrier cyclicBarrier){
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     public void arrive(){
@@ -47,22 +51,31 @@ class MeetingCyclicBarrier implements Runnable{
     }
 }
 
+class LastRunner implements Runnable{
+    public void run() {
+        System.out.println("last running");
+    }
+}
+
 class ParterCyclicBarrier implements Runnable{
     private String name;
-    private Meeting meeting;
+    private MeetingCyclicBarrier meeting;
 
-    ParterCyclicBarrier(String name, Meeting meeting){
+    ParterCyclicBarrier(String name,
+                        MeetingCyclicBarrier meeting){
         this.name = name;
         this.meeting = meeting;
     }
     public void run() {
 
         long dura = (long) Math.random()*100;
+        /*
         try{
             Thread.sleep(dura);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
+        */
         meeting.arrive();
     }
 }
