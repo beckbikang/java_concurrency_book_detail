fe7fc0d7f3ae07bcff86b13f07b026346618e28a
毕康 <bikang1@staff.weibo.com>
2018-10-06 11:13:12 +0800

    新增代码

---
+++ src/main/java/Java/logic/part4/PutstreamTest.java
@@ -0,0 +1,34 @@
+package Java.logic.part4;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class PutstreamTest {
+
+    public static void main(String[] args){
+        woutput();
+    }
+
+    /**
+     * 输出数据
+     * 
+     */
+    public static void woutput(){
+
+        OutputStream outputStream;
+        try{
+            outputStream = new FileOutputStream("tt.log");
+            String data = "abc";
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("end");
+        }
+    }
+
+    public static void tt(){
+        System.out.println("tt");
+    }
+}
