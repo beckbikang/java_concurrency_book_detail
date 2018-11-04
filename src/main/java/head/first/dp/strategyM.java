58495b674165b159a3d494096b9ccac9dcb80a80
毕康 <bikang1@staff.weibo.com>
2018-11-04 18:00:34 +0800

    更加灵活

---
+++ src/main/java/head/first/dp/strategyM.java
@@ -0,0 +1,91 @@
+package head.first.dp;
+
+public class strategyM {
+
+    public static void main(String[] args){
+
+        MallardDuck mallardDuck = new MallardDuck();
+        mallardDuck.display();
+
+        DecoyDuck decoyDuck = new DecoyDuck();
+        decoyDuck.display();
+
+
+
+    }
+}
+
+
+interface Flyable{
+    void fly();
+}
+
+interface Quackable{
+    void quack();
+}
+
+class Ducker{
+
+    Flyable flyable;
+    Quackable quackable;
+
+    public void swim(){
+
+    }
+    public void display(){
+        quack();
+        fly();
+    }
+
+    public void setFlyable(Flyable flyable){
+        this.flyable = flyable;
+    }
+
+    public void setQuackable(Quackable quackable){
+        this.quackable = quackable;
+    }
+
+    public void quack(){
+        if(quackable != null)
+        quackable.quack();
+    }
+
+    public void fly(){
+        if(flyable != null)
+        flyable.fly();
+    }
+}
+
+class DecoyDuck extends Ducker{
+    public void display(){
+        System.out.println("nothing");
+    }
+}
+
+class QuackMouth implements Quackable{
+    @Override
+    public void quack(){
+        System.out.println("quack with mouth");
+    }
+}
+
+class FlySwing implements Flyable{
+    @Override
+    public void fly() {
+        System.out.println("fly with swing");
+    }
+}
+
+class MallardDuck extends Ducker{
+
+    MallardDuck(){
+        setFlyable(new FlySwing());
+        setQuackable(new QuackMouth());
+    }
+
+    public void display(){
+        super.display();
+        System.out.println("MallardDuck display");
+    }
+
+}
---
+++ src/main/java/head/first/dp/strategyM1.java
@@ -0,0 +1,45 @@
+package head.first.dp;
+
+public class strategyM1 {
+
+    public static void main(String[] args){
+        
+    }
+}
+
+interface Flyable1{
+    void fly();
+}
+
+interface Quackable1{
+    void quack();
+}
+
+class Ducker1{
+    public void swim(){
+
+    }
+    public void display(){
+
+    }
+}
+
+class DecoyDuck1 extends Ducker1{
+    public void display(){
+
+    }
+}
+
+class MallardDuck1 extends Ducker1 implements Flyable1,Quackable1{
+
+    public void fly(){
+
+    }
+    public void display(){
+
+    }
+
+    public void quack(){
+
+    }
+}
