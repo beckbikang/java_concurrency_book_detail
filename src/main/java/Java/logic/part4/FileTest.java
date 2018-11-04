8f461c416a8d326ea98394a81999ada1e1d96761
毕康 <bikang1@staff.weibo.com>
2018-11-04 17:44:42 +0800

    go

---
+++ src/main/java/Java/logic/part4/FileTest.java
@@ -0,0 +1,12 @@
+package Java.logic.part4;
+
+public class FileTest {
+
+    public static void main(String[] args){
+        tt();
+    }
+
+    public static void tt(){
+        System.out.println("tt");
+    }
+}
